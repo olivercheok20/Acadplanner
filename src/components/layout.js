d0abde4a486295d6ed47c36e0c6001c85d249674
Oliver Cheok <olivercheok20@gmail.com>
2020-11-03 18:18:13 +0800

    Edit layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -16,7 +16,7 @@ export default class Layout extends Component {
         return (
             <Fragment>
                 <Row style={{"margin" : 0, "height" : "8vh", "border-bottom" : "1px solid #DDDDDD"}}>
-                    <Col lg={3} style={{"position": "relative"}}>
+                    <Col lg={2} style={{"position": "relative"}}>
                         <h2 style={
                             {"position" : "absolute", 
                             "top" : "50%", "left": "50%", "transform": "translate(-50%, -50%)", 
@@ -25,15 +25,15 @@ export default class Layout extends Component {
                             Acadplanner
                         </h2>
                     </Col>
-                    <Col lg={9} style={{"padding" : 0}}>
+                    <Col lg={10} style={{"padding" : 0}}>
                     </Col>
                 </Row>
                 <Row style={{"margin" : 0}}>
-                    <Col lg={3} style={{"padding-left" : 15, "padding-right" : 15, "padding-top" : 10, "height" : "92vh"}}>
+                    <Col lg={2} style={{"padding-left" : 15, "padding-right" : 15, "padding-top" : 10, "height" : "92vh"}}>
                         <MetisMenu content={MainNav} activeLinkFromLocation
                         className="vertical-nav-menu" iconNamePrefix=""/>
                     </Col>
-                    <Col lg={9} style={{"padding" : 0, "border-left" : "1.5px solid #DDDDDD"}}>
+                    <Col lg={10} style={{"padding" : 0, "border-left" : "1.5px solid #DDDDDD"}}>
                         {this.props.children}
                     </Col>
                 </Row>
