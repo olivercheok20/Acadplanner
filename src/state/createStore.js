787932c1852d3cc497a374fc94b1a1693cba9e8b
Oliver Cheok <olivercheok20@gmail.com>
2020-11-08 19:37:57 +0800

    Create store and implement in layout

--- src/state/createStore.js
+++ src/state/createStore.js
@@ -1,14 +1,23 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 
-function reducer() {
-  //...
+function profileReducer(profile = { 'name': 'Oliver Cheok' }, action) {
+    switch (action.type) {
+        default:
+            return profile
+    }
 }
+
+const rootReducer = combineReducers({ profile: profileReducer})
 
 // preloadedState will be passed in by the plugin
 export default preloadedState => {
-  return createStore(reducer, preloadedState, compose(
-    applyMiddleware(thunk),
-    window.devToolsExtension ? window.devToolsExtension() : f => f
-  ));
+    return createStore(
+        rootReducer,
+        preloadedState,
+        compose(
+            applyMiddleware(thunk),
+            window.devToolsExtension ? window.devToolsExtension() : f => f
+        )
+    );
 };
