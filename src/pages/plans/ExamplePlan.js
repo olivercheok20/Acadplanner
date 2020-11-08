e419bb7d10be107460914f4a3fed30a83dd03d00
Oliver Cheok <olivercheok20@gmail.com>
2020-11-09 03:53:05 +0800

    Connect ExamplePlan page to Redux

--- src/pages/plans/ExamplePlan.js
+++ src/pages/plans/ExamplePlan.js
@@ -1,175 +1,142 @@
-import React, { useState, useEffect } from "react"
+import React, { Component } from "react"
 import Layout from "../../components/layout"
 import { Semester } from "../../components/Semester";
 import { PlanToTake } from "../../components/PlanToTake";
 import { Row, Col, DropdownToggle, DropdownMenu, DropdownItem, UncontrolledDropdown, Input, Button } from "reactstrap";
 
-export default function ExamplePlan() {
+import { connect, Provider } from "react-redux";
+import store from '../../state/createStore';
 
-  const [user, setUser] = useState({});
-  const [plans, setPlans] = useState([]);
-  const [semesters, setSemesters] = useState([]);
-  const [planToTakeModules, setPlanToTakeModules] = useState([]);
-  const [planName, setPlanName] = useState('');
-  const [planDescription, setPlanDescription] = useState('');
-  const [editPlanName, setEditPlanName] = useState(false);
-  const [editPlanDescription, setEditPlanDescription] = useState(false);
+class ExamplePlan extends Component {
 
-  useEffect(() => {
-    let retrievedUser1 = {  // user who already has plans
-      name: "Oliver Cheok",
-      plans: [
-        {
-          planName: 'CS + USP + Israel Exchange',
-          planDescription: 'This plan is for a CS major specialising in artificial intelligence and computer security + USP + an exchange to Israel in Y3S2.',
-          semesters: [
-            { semesterName: 'sem1', modules: [{ name: 'CS1010', modularCredits: '4', grade: 'A' }, { name: 'CS2030', modularCredits: '4', grade: 'A' }] },
-            { semesterName: 'sem2', modules: [{ name: 'CS2040', modularCredits: '4', grade: 'A' }] },
-            { semesterName: 'sem3', modules: [{ name: 'MA1521', modularCredits: '4', grade: 'A' }] }
-          ],
-          planToTakeModules: [
-            { name: 'CS1231', modularCredits: '4' }
-          ]
-        },
-        {
-          planName: 'Plan 2',
-          planDescription: 'This is plan 2.',
-          semesters: [
-            { semesterName: 'sem1', modules: [{ name: 'GEQ1000', modularCredits: '4', grade: 'A' }, { name: 'GEH1000', modularCredits: '4', grade: 'A' }] },
-            { semesterName: 'sem2', modules: [{ name: 'GET1000', modularCredits: '4', grade: 'A' }] },
-            { semesterName: 'sem3', modules: [{ name: 'GER1000', modularCredits: '4', grade: 'A' }] }
-          ],
-          planToTakeModules: [
-            { name: 'GES1000', modularCredits: '4' }
-          ]
-        }
-      ]
-    };
-
-    // let retrievedUser2 = {  // new user who does not have any plans
-    //   name: "Jefferson Low",
-    //   plans: []
-    // };
-
-    setUser(retrievedUser1);
-    setPlans(retrievedUser1.plans)
-    setSemesters(retrievedUser1.plans[0].semesters);
-    setPlanToTakeModules(retrievedUser1.plans[0].planToTakeModules);
-    setPlanName(retrievedUser1.plans[0].planName);
-    setPlanDescription(retrievedUser1.plans[0].planDescription);
-  }, [])
-
-  // const addPlan = () => {
-  //   setPlans([...plans,
-  //   {
-  //     planName: 'Plan ' + plans.length,
-  //     planDescription: 'New plan description',
-  //     semesters: [{ semesterName: 'New semester', modules: [{ name: '', modularCredits: '', grade: '' }] }],
-  //     planToTakeModules: [{ name: '', modularCredits: '', }]
-  //   }
-  //   ]);
-  // }
-
-  const onChangePlanName = e => {
-    setPlanName(e.target.value);
+  constructor(props) {
+    super(props);
+    this.state = {
+      editPlanDescription: false,
+      editPlanName: false,
+      activePlan: this.props.plans[0]
+    }
   }
 
-  const onChangePlanDescription = e => {
-    setPlanDescription(e.target.value);
+  getNamesFromPlans() {
+    var names = []
+    this.props.plans.forEach(function (plan, index) {
+      names.push(plan.planName)
+    })
+    return names
   }
 
-  const addSemester = () => {
-    setSemesters([...semesters, { semesterName: 'New semester', modules: [{ name: '', modularCredits: '', grade: '' }] }]);
+  handleChangePlan(planName) {
+    var newActivePlan = null
+
+    this.props.plans.forEach(function (plan, index) {
+      if (plan.planName == planName) {
+        newActivePlan = plan
+      }
+    }
+    )
+    this.setState({ activePlan: newActivePlan });
   }
 
-  const handleChangePlan = (planName) => {
-    // let selectedPlan = plans.filter(plan => plan.planName == planName)[0];
-    // console.log('before: ' + semesters[0].modules[0].name);
-    // console.log('to change to: ' + selectedPlan.semesters[0].modules[0].name)
-    // setSemesters(selectedPlan.semesters);
-    // setPlanToTakeModules(selectedPlan.planToTakeModules);
-    // setPlanName(selectedPlan.planName);
-    // setPlanDescription(selectedPlan.planDescription);
+  addSemester() {
+
   }
 
-  return (
-    <Layout>
-
-      <UncontrolledDropdown style={{ marginLeft: 60, marginBottom: 50 }} >
-        <DropdownToggle caret style={{ width: 500, backgroundColor: 'white', color: "black", textAlign: "left" }} >
-          {planName}
-        </DropdownToggle>
-        <DropdownMenu>
-          <DropdownItem header>My Plans</DropdownItem>
-          {plans.map((plan, i) => (
-            <DropdownItem onClick={() => handleChangePlan(plan.planName)} key={i}>{plan.planName} </DropdownItem>
-          ))}
-          <DropdownItem divider />
-          <DropdownItem>Add new plan</DropdownItem>
-        </DropdownMenu>
-      </UncontrolledDropdown>
-
-      <Row style={{ marginLeft: 60, marginBottom: 30, height: 100 }}>
-        <Col md={3}>
-          <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-            <h5>Name</h5>
-            {!editPlanName && <Button color="link" onClick={() => setEditPlanName(true)}>edit</Button>}
-            {editPlanName && <Button color="link" onClick={() => setEditPlanName(false)}>done</Button>}
-          </div>
-          {!editPlanName && <p>{planName}</p>}
-          {editPlanName && <Input
-            type="textarea"
+  render() {
+    return (
+      <Layout>
+        <Provider store={store()}>
+          <Input
+            type="select"
             name="text"
-            id="exampleText"
-            value={planName}
-            onChange={onChangePlanName}
-            rows={2}
-          />}
-        </Col>
+            id="planselection"
+            defaultValue={this.getNamesFromPlans[0]}
+            onChange={(e) => { this.handleChangePlan(document.getElementById("planselection").value) }}
+          >
+            {this.props.plans.map((plan, i) => (
+              <option key={i}>{plan.planName} </option>
+            ))}
+          </Input>
 
-        <Col md={5}>
-          <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-            <h5>Description</h5>
-            {!editPlanDescription && <Button color="link" onClick={() => setEditPlanDescription(true)}>edit</Button>}
-            {editPlanDescription && <Button color="link" onClick={() => setEditPlanDescription(false)}>done</Button>}
-          </div>
-          {!editPlanDescription && <p>{planDescription}</p>}
-          {editPlanDescription && <Input
-            type="textarea"
-            name="text"
-            id="exampleText"
-            value={planDescription}
-            onChange={onChangePlanDescription}
-            rows={3}
-          />}
-        </Col>
-      </Row>
 
-      <h3 style={{ marginLeft: 50 }}>Semesters</h3>
+          <Row style={{ marginLeft: 60, marginBottom: 30, height: 100 }}>
+            <Col md={3}>
+              <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                <h5>Name</h5>
+                {!this.state.editPlanName && <Button color="link" onClick={() => this.setState({ editPlanName: true })}>edit</Button>}
+                {this.state.editPlanName && <Button color="link" onClick={() => this.setState({ editPlanName: false })}>done</Button>}
+              </div>
+              {!this.state.editPlanName && this.state.activePlan && <p>{this.state.activePlan.planName}</p>}
+              {this.state.editPlanName && <Input
+                type="textarea"
+                name="text"
+                id="exampleText"
+                value={this.state.activePlan.planName}
+                onSubmit={this.props.onChangePlanName}
+                rows={2}
+              />}
+            </Col>
 
-      <Row style={{ margin: 30, marginTop: 0 }}>
-        {semesters.map((semester, i) => (
-          <Col md={6} key={i}>
-            <div style={{ margin: 20 }} >
-              <Semester semester={semester} />
-            </div>
-          </Col>
-        ))}
-        <Col md={6}>
-          <div style={{ margin: 20, flex: 1 }}>
-            <Button style={{ width: '100%' }} color="info" onClick={() => addSemester()}>Add Semester</Button>
-          </div>
-        </Col>
-      </Row>
+            <Col md={5}>
+              <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                <h5>Description</h5>
+                {!this.state.editPlanDescription && <Button color="link" onClick={() => this.setState({ editPlanDescription: true })}>edit</Button>}
+                {this.state.editPlanDescription && <Button color="link" onClick={() => this.setState({ editPlanDescription: false })}>done</Button>}
+              </div>
+              {!this.state.editPlanDescription && this.state.activePlan && <p>{this.state.activePlan.planDescription}</p>}
+              {this.state.editPlanDescription && <Input
+                type="textarea"
+                name="text"
+                id="exampleText"
+                value={this.state.activePlan.planDescription}
+                onChange={this.props.onChangePlanDescription}
+                rows={3}
+              />}
+            </Col>
+          </Row>
 
-      <h3 style={{ marginLeft: 50 }}>Plan to take</h3>
-      <Row style={{ margin: 30, marginTop: 0 }}>
-        <Col md={6} style={{ margin: 20 }}>
-          <PlanToTake planToTakeModules={planToTakeModules} />
-        </Col>
-      </Row>
+          <h3 style={{ marginLeft: 50 }}>Semesters</h3>
 
-      <hr style={{ margin: 50, marginBottom: 100 }} />
-    </Layout>
-  )
+          <Row style={{ margin: 30, marginTop: 0 }}>
+            {this.state.activePlan && this.state.activePlan.years[0].semesters.map((semester, i) => ( // THIS IS ONLY SHOWING THE SEMESTERS FOR THE FIRST YEAR, MUST USE FOREACH YEAR TO SHOW SEMESTERS FOR ALL YEARS
+              <Col md={6} key={i}>
+                <div style={{ margin: 20 }} >
+                  <Semester semester={semester} />
+                </div>
+              </Col>
+            ))}
+            <Col md={6}>
+              <div style={{ margin: 20, flex: 1 }}>
+                <Button style={{ width: '100%' }} color="info" onClick={() => this.addSemester()}>Add Semester</Button>
+              </div>
+            </Col>
+          </Row>
+
+          <h3 style={{ marginLeft: 50 }}>Plan to take</h3>
+          <Row style={{ margin: 30, marginTop: 0 }}>
+            <Col md={6} style={{ margin: 20 }}>
+              {this.state.activePlan && <PlanToTake planToTakeModules={this.state.activePlan.planToTakeModules} />}
+            </Col>
+          </Row>
+
+          <hr style={{ margin: 50, marginBottom: 100 }} />
+        </Provider>
+      </Layout>
+    )
+  }
 }
+
+function mapState(state) {
+  return { plans: state.plans }
+}
+
+// Maps `dispatch` to `props`:
+function mapDispatch(dispatch) {
+  return {
+    onChangePlanName: (planName, newName) => dispatch ({type: 'changePlanName', payload: { 'planName': planName, 'newName': newName }}),
+    onChangePlanDescription: (planName, newDescription) => dispatch ({type: 'changePlanName', payload: { 'planName': planName, 'newDescription': newDescription }})
+  }
+}
+
+// Connect them:
+export default connect(mapState, mapDispatch)(ExamplePlan)
