e70687eff55a1413d826aadb3b26f80bfff543c9
Oliver Cheok <olivercheok20@gmail.com>
2020-11-08 18:21:47 +0800

    Format layout to include profile link

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,37 +3,50 @@ import React, { Fragment, Component } from "react";
 import { Navbar, Nav, NavbarBrand, NavItem, NavLink, Row, Col } from "reactstrap";
 import MetisMenu from "react-metismenu";
 
-import {
-    MainNav,
-    ComponentsNav,
-    FormsNav,
-    WidgetsNav,
-    ChartsNav,
-  } from "./NavItems";
+import { connect } from "react-redux";
 
-export default class Layout extends Component {
+import {
+    MainNav
+} from "./NavItems";
+
+class Layout extends Component {
     render() {
         return (
             <Fragment>
-                <Row style={{"margin" : 0, "height" : "8vh", "border-bottom" : "1px solid #DDDDDD"}}>
-                    <Col lg={2} style={{"position": "relative"}}>
-                        <h2 style={
-                            {"position" : "absolute", 
-                            "top" : "50%", "left": "50%", "transform": "translate(-50%, -50%)", 
-                            "margin-bottom" : 0}
+                <Row style={{ "margin": 0, "height": "8vh", "border-bottom": "1px solid #DDDDDD" }}>
+                    <Col lg={2} style={{ "position": "relative" }}>
+                        <a href='/plans'>
+                            <h2 style={
+                                {
+                                    "position": "absolute",
+                                    "top": "50%", "left": "50%", "transform": "translate(-50%, -50%)",
+                                    "margin-bottom": 0,
+                                    "color": '#495057'
+                                }
                             }>
-                            Acadplanner
+                                Acadplanner
                         </h2>
+                        </a>
                     </Col>
-                    <Col lg={10} style={{"padding" : 0}}>
+                    <Col lg={10} style={{ "padding": 0 }}>
+                        <div style={{ display: 'flex', justifyContent: 'space-between', 'padding-left': 20, 'padding-right': 20, alignItems: 'center', height: '8vh' }}>
+                            <div></div>
+
+                            <div style={{ display: 'flex', alignItems: 'center' }}>
+                                <a href='/profile'>
+                                    <a style={{ marginRight: 10, color: '#495057' }} href='/profile'>{"Oliver Cheok"}</a>
+                                    <img src={require("../assets/avatar-icon.png")} height='50' />
+                                </a>
+                            </div>
+                        </div>
                     </Col>
                 </Row>
-                <Row style={{"margin" : 0}}>
-                    <Col lg={2} style={{"padding-left" : 15, "padding-right" : 15, "padding-top" : 10, "height" : "92vh"}}>
+                <Row style={{ "margin": 0 }}>
+                    <Col lg={2} style={{ "padding-left": 15, "padding-right": 15, "padding-top": 10, "height": "92vh" }}>
                         <MetisMenu content={MainNav} activeLinkFromLocation
-                        className="vertical-nav-menu" iconNamePrefix=""/>
+                            className="vertical-nav-menu" iconNamePrefix="" />
                     </Col>
-                    <Col lg={10} style={{"padding" : 0, "border-left" : "1.5px solid #DDDDDD"}}>
+                    <Col lg={10} style={{ "padding": 25, "border-left": "1.5px solid #DDDDDD" }}>
                         {this.props.children}
                     </Col>
                 </Row>
@@ -41,3 +54,5 @@ export default class Layout extends Component {
         )
     }
 }
+
+export default connect()(Layout)
