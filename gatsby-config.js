da866f3015c17054b45918b695c265cfbeba47a5
Oliver Cheok <olivercheok20@gmail.com>
2020-11-10 16:14:31 +0800

    Test client side rendering

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,7 +33,7 @@ module.exports = {
     },
     {
       resolve: `gatsby-plugin-create-client-paths`,
-      options: { prefixes: [ `/, ``/index/*`] },
+      options: { prefixes: [ `/`, `/index/*`] },
     },
   ],
 }
